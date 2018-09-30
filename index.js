09d0311518f40c7029019c9ed4f414ff93ac4c41
leofavre <contato@leofavre.com>
2018-09-30 10:20:23 -0300

    Remove not implemented commands

--- index.js
+++ index.js
@@ -3,16 +3,11 @@ const yargs = require('yargs');
 const { existsSync } = require('fs-extra');
 const { resolve, relative } = require('path');
 
-const CMD_BUILD_DESC = 'Build packages';
 const CMD_START_DESC = 'Start development server';
-const CMD_SCREEN_TEST_DESC = 'Test interface';
-const CMD_TEST_DESC = 'Test packages';
 const INVALID_SCOPE = 'Invalid scope';
 const MISSING_COMMAND = 'Please choose a command';
 const MISSING_SCOPE = 'Please choose a scope';
 const OPT_BASE_DESC = 'Path to the packages\' folder';
-const OPT_DEBUG_DESC = 'Run in debug mode';
-const OPT_DEPLOY_DESC = 'Prepare for deployment';
 const OPT_SCOPE_DESC = 'Package affected by script';
 
 const POSITIONAL_SCOPE = {
@@ -38,21 +33,6 @@ const argv = yargs
         demandOption: MISSING_SCOPE
       });
   })
-  .command('build', CMD_BUILD_DESC, {
-    deploy: {
-      alias: 'D',
-      boolean: true,
-      description: OPT_DEPLOY_DESC
-    }
-  })
-  .command('test', CMD_TEST_DESC, {
-    debug: {
-      alias: 'd',
-      boolean: true,
-      description: OPT_DEBUG_DESC
-    }
-  })
-  .command('screen-test', CMD_SCREEN_TEST_DESC)
   .demandCommand(1, 1, MISSING_COMMAND)
   .alias('h', 'help')
   .help()
